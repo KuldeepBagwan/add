ea38f2c29101e0c19e50fc29f43c64c5f9d18a50
KuldeepBagwan <163808960+KuldeepBagwan@users.noreply.github.com>
2024-03-18 10:09:42 +0530

    Add files via upload
    this is 1st change

---
+++ Addition.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+int num1,num2,sum;
+cout<<"Enter your 1st number = ";
+cin>>num1;
+cout<<"Enter your 2nd number = ";
+cin>>num2;
+sum=num1+num2;
+  cout<<"Summation is = "<<sum<<endl;
+
+return 0;
+}
